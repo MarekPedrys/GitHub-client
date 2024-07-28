437e815b0bd2344d466169c9c1e027ebecb1144a
Marek Pedrys <marek.pedrys@gmail.com>
2024-07-28 22:50:15 +0200

    Initial commit

---
+++ src/main/java/pl/marekpedrys/githubclient/GitHubClientApplication.java
@@ -0,0 +1,13 @@
+package pl.marekpedrys.githubclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/marekpedrys/githubclient/GitHubClientApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marekpedrys.githubclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
